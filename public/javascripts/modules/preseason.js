ad5dd15eaf560a293fb3f51ea251190436196fb4
Tommy Tate <tommy.tate@ad.newspring.cc>
2019-07-29 16:23:27 -0400

    updated preseason division points

--- public/javascripts/modules/preseason.js
+++ public/javascripts/modules/preseason.js
@@ -1,23 +1,29 @@
 const buttons = document.querySelectorAll('.team');
 let champNum = 0;
 
-buttons.forEach(button => button.addEventListener('click', function(e) {
-  e.preventDefault;
-  const name = this.getAttribute('name');
+function selectWinner(button, hide=true) {
+  const name = button.getAttribute('name');
   const matchUp = document.getElementsByName(name);
-  const teamName = this.getAttribute('value');
+  const teamName = button.getAttribute('value');
   const teamRadios = document.querySelectorAll(`input[value="${teamName}"]`);
   teamRadios.forEach(teamRadio => teamRadio.checked = true);
-  matchUp.forEach(team => {
-    team.classList.remove('active');
-    team.classList.toggle('hidden');
-  });
-  this.classList.add('active');
-  this.classList.toggle('hidden');
+  if(hide) {
+    matchUp.forEach(team => {
+      team.classList.remove('active');
+      team.classList.toggle('hidden');
+    });
+    button.classList.add('active');
+    button.classList.toggle('hidden');
+  } else {
+    matchUp.forEach(team => {
+      team.classList.remove('active');
+      team.classList.remove('hidden');
+    });
+  }
 
   const pointsDisplay = document.querySelectorAll('.points');
   pointsDisplay.forEach((pointDisplay, i) => {
-    if(pointDisplay.getAttribute('id') === this.getAttribute('name')) {
+    if(pointDisplay.getAttribute('id') === button.getAttribute('name')) {
       pointDisplay.classList.toggle('hiddenPoints');
       if(`${teamName.split(" ").length}` > 1) {
         pointDisplay.classList.toggle(`${teamName.split(" ")[0]}`);
@@ -25,19 +31,31 @@ buttons.forEach(button => button.addEventListener('click', function(e) {
       } else {
         pointDisplay.classList.toggle(`${teamName}`);
       };
-      pointDisplay.innerHTML = `Worth ${this.getAttribute('data-points')} Point(s)`;
+      pointDisplay.innerHTML = `Worth ${button.getAttribute('data-points')} Point(s)`;
       champNum = ((i+1) % 3)
-    }
 
+      if(!hide) {
+        pointDisplay.classList = 'points hiddenPoints';
+      }
+    }
+    
   });
 
   const pointsInput = document.getElementsByName(`${name}Points`);
   pointsInput.forEach(point => {
-    point.setAttribute('value', `${this.getAttribute('data-points')}`);
+    point.setAttribute('value', `${button.getAttribute('data-points')}`);
   });
+}
+
+buttons.forEach(button => button.addEventListener('click', function(e) {
+  e.preventDefault;
+  const name = this.getAttribute('name');
+  const teamName = this.getAttribute('value');
+  selectWinner(this);
 
   // Championship Games
   const championshipTeams = document.querySelectorAll(`.${this.getAttribute('data-conference')}`);
+  console.log(champNum);
   if(champNum !== 0) {
     const divisionLabel = (championshipTeams[(champNum-1)*2]);
     const divisionInput = (championshipTeams[(champNum-1)*2+1]);
@@ -50,13 +68,19 @@ buttons.forEach(button => button.addEventListener('click', function(e) {
     } else {
       divisionLabel.classList.toggle(`${teamName}`);
     };
+    divisionLabel.classList.remove('active');
     divisionInput.value = `${teamName}`;
+    console.log(divisionLabel);
+    selectWinner(divisionLabel, false);
+  
   }
+  
 
-  const champPoints = document.querySelectorAll(`input[name="${this.getAttribute('data-conference')}Points"]`);
-  champPoints.forEach(champPoint => champPoint.setAttribute('value', `${this.getAttribute('data-points')}`));
-  const champPointsDisplay = document.getElementById(`${this.getAttribute('data-conference')}`);
-  console.log(this.getAttribute('data-points'))
-  champPointsDisplay.innerHTML = `Worth ${this.getAttribute('data-points')} Points`
+  // champPoints.forEach(champPoint => champPoint.addEventListener('change', (e) => {
+  //   console.log(e);
+  // }));
+  // champPoints.forEach(champPoint => champPoint.setAttribute('value', `${this.getAttribute('data-points')}`));
+  // const champPointsDisplay = document.getElementById(`${this.getAttribute('data-conference')}`);
+  // champPointsDisplay.innerHTML = `Worth ${this.getAttribute('data-points')} Points`;
 }));
 
