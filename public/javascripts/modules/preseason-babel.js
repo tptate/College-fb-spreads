492e48d26fc4344ca68d62a2cf795cffd168044b
Tommy Tate <tommy.tate@ad.newspring.cc>
2019-08-04 20:59:36 -0400

    mobile preseason v1.11

--- public/javascripts/modules/preseason-babel.js
+++ public/javascripts/modules/preseason-babel.js
@@ -93,9 +93,9 @@ function selectWinner(button) {
   }
 }
 
-function selectTeam(button, teamName, division, conference) {
+function selectTeam(button, teamName, division, conference, teamRadio) {
   button.classList.toggle('active');
-  // teamRadio.checked ? teamRadio.checked = false : teamRadio.checked = true;
+  teamRadio.checked ? teamRadio.checked = false : teamRadio.checked = true;
   // pointDisplay.classList.toggle('hiddenPoints');
   // pointDisplay.innerHTML = `<p>Worth ${points} Point(s)</p>`;
   // if(`${teamName.split(" ").length}` > 1) {
@@ -117,14 +117,14 @@ buttons.forEach(button => button.addEventListener('mousedown', function(e) {
   const teamName = button.getAttribute('value');
   const division = button.getAttribute('name');
   const conference = this.getAttribute('data-conference');
-  // // const teamRadio = document.querySelector(`input[value="${teamName}"][name="${division}"]`);
+  const teamRadio = document.querySelector(`input[value="${teamName}"][name="${division}"]`);
   // const pointDisplay = document.querySelector(`#${division}`);
   // const points = this.getAttribute('data-points');
   // const pointInput = document.querySelector(`input[name=${this.getAttribute('for')}Points`);
   
   this.classList.contains('active') ? selected = false : selected = true;
   matchUp.forEach(team => {
-    team === this ? selectTeam(this, teamName, division, conference) : hideTeam(team);
+    team === this ? selectTeam(this, teamName, division, conference, teamRadio) : hideTeam(team);
   });
 
   // const division = this.getAttribute('for');
