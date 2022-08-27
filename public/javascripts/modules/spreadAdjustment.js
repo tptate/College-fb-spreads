849202a3229f7d0116f11672c33bb5f149b9aebb
Tommy Tate <Tommy.Tate@newspring.cc>
2022-08-27 19:28:27 -0400

    The Spread Picks Club v22.001

--- public/javascripts/modules/spreadAdjustment.js
+++ public/javascripts/modules/spreadAdjustment.js
@@ -1,6 +1,26 @@
 const spreadButton = document.querySelector('input[name="spread"]');
 const spreadWords = document.querySelector('label[for="spread"]');
-let spread = 0;
+// const homeButton = document.querySelector('input[name="home"]');
+// // const homeTeam = homeButton.value;
+// const awayButton = document.querySelector('input[name="away"]');
+// // const awayTeam = awayButton.value;
+// let spread = 0;
+// let home = "";
+// let away = "";
+// let favorite = "";
+
+// homeButton.addEventListener('change', function(e) {
+//   home = parseInt(this.value);
+// };
+
+// awayButton.addEventListener('change', function(e) {
+//   away = parseInt(this.value);
+// };
+
+// spreadButton.addEventListener('change', function(e) {
+//   this.value > 0 ? spread = parseInt(this.value) + 0.5 : spread = parseInt(this.value) - 0.5;
+//   this.value > 0 ? favorite = home + spread : favorite = away + spread;
+//   spreadWords.innerHTML = `Point Spread: ${favorite}`
 
 spreadButton.addEventListener('change', function(e) {
   this.value > 0 ? spread = parseInt(this.value) + 0.5 : spread = parseInt(this.value) - 0.5;
